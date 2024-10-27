9469c38f07a3b952c199faad50799bffe66d1b48
Real Idiot <76228912+Yebonow@users.noreply.github.com>
2024-10-27 12:05:09 +0000

    Update grabBH.php
    not sure if this works but if not feel free to stab me

--- grabBH.php
+++ grabBH.php
@@ -1,34 +1,83 @@
 <?php
-ob_start();
+// CHECK AVAILABILITY OF THE API SITE
+// check if reachable:
+if (fsockopen("https://api.brick-hill.com",443,$errno,$error,$time)) {
+	// reachable by server
+} else {
+	// host not reachable
+	exit('<html><head><title></title><script>alert("It seems that Brick Hill\'s API is not currently reachable; this tool cannot be used.");</script></head><body></body></html>');
+}
+// check http status 
+$_ = curl_init("https://api.brick-hill.com");
+curl_exec($_);
+$status = curl_getinfo($_,CURLINFO_HTTP_CODE);
+curl_close($_);
+if ($status != 200) {
+	// status could be bad gateway, or some other cloudflare error, meaning the site is down.
+	exit('<html><head><title></title><script>alert("It seems that Brick Hill\'s API is not currently available; this tool cannot be used.");</script></head><body></body></html>');
+} else {
+	// the server returned 200, success status so we do not exit
+}
+ob_start(); // to control output
 if (isset($_POST['texture'])) {
+	// parse JSON for texture value
 	$jsonResp = json_decode(file_get_contents('https://api.brick-hill.com/v1/assets/getPoly/1/'.$_POST['itemID']));
-	if (isset($jsonResp[0]->texture)) {
+	if (isset($jsonResp[0]->texture)) {// if a texture even exists
+		// set the corresponding content type for the image we are downloading
 		header('Content-Type: image/png');
+		// tell the browser that we are downloading a file, and tell it what to download the file as.
 		header('Content-Disposition: attachment; filename="'.$_POST['itemID'].'.png"');
 		ob_clean();
 		flush();
-		readfile('https://api.brick-hill.com/v1/assets/get/'.substr($jsonResp[0]->texture,8));
+		// the first 8 chars of the value is asset://, which is not needed by the get api, so we skip them
+		$assetID = substr($jsonResp[0]->texture,8);
+		// get asset, output to http stream
+		readfile('https://api.brick-hill.com/v1/assets/get/'.$assetID);
 		exit();
-	} else {
-		print('<script>alert("No texture available.")</script>'."\n");
+	} else {// no texture value
+		$ext = '<script>alert("No texture available.")</script>';
 	}
 }
 if (isset($_POST['mesh'])) {
+	// parse JSON for mesh value
 	$jsonResp = json_decode(file_get_contents('https://api.brick-hill.com/v1/assets/getPoly/1/'.$_POST['itemID']));
-	if (isset($jsonResp[0]->mesh)) {
+	if (isset($jsonResp[0]->mesh)) {// if a mesh even exists
+		//set a generic content type for an obj file
 		header('Content-Type: application/octet-stream');
+		// tell the browser that we are downloading the file, and tell it what to download the file as
 		header('Content-Disposition: attachment; filename="'.$_POST['itemID'].'.obj"');
 		ob_clean();
 		flush();
-		readfile('https://api.brick-hill.com/v1/assets/get/'.substr($jsonResp[0]->mesh,8));
+		// the first 8 chars of the value is asset://, which is not needed by the get api, so we skip them
+		$assetID = substr($jsonResp[0]->mesh,8);
+		// get asset, output to http stream
+		readfile('https://api.brick-hill.com/v1/assets/get/'.$assetID);
 		exit();
-	} else {
-		print('<script>alert("No mesh available.")</script>'."\n");
+	} else {// no mesh value
+		$ext = '<script>alert("No mesh available.")</script>';
 	}
 }
 ?>
-<form method="post">
-	<input type="text" name="itemID" placeholder="Item ID" value="<?=@$_POST['itemID']?>">
-	<input type="submit" value="Texture" name="texture">
-	<input type="submit" value="Mesh" name="mesh">
-</form>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta charset="UTF-8">
+		<meta name="robots" content="noindex">
+		<title>Brick Hill Asset Grabber</title>
+		<style type="text/css">
+			* {
+				font-family: Arial, sans-serif;
+			}
+		</style>
+		<?=@$ext?>
+	</head>
+	<body>
+		<form method="post">
+			<input type="text" name="itemID" placeholder="Item ID" value="<?=@$_POST['itemID']?>">
+			<input type="submit" value="Texture" name="texture">
+			<input type="submit" value="Mesh" name="mesh">
+		</form>
+	</body>
+</html>
